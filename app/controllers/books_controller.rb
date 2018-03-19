c7642e0e16697dfd25cb8ccf97060598d8f0bde5
Kari Bancroft <kariabancroft@gmail.com>
2018-03-19 15:56:23 -0700

    add erb code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
   def index
     # Placeholder book data until we get models tomorrow!
+    # @my_string = "yelling"
+
     @books = [
       { title: "Hidden Figures", author: "Margot Lee Shetterly"},
       { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-      { title: "Kindred", author: "Octavia E. Butler"}
+      { title: "Kindred", author: "Octavia E. Butler"},
+      { title: "Uncreative", author: "Book"}
     ]
   end
 
