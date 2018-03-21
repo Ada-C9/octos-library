563122bff57afafd630be1ba3bcec95e7674a45a
Dan Roberts <dan@adadevelopersacademy.org>
2018-03-21 14:53:12 -0700

    Implement create, add the edit form, implement update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,20 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(title: params[:my_title], author: params[:author])
+    # Rails form_for creates a nested param structure
+    # This is fancy and good, but means we need to do
+    # a little extra work.
+    raw_book = params[:book]
+
+    book = Book.new
+    book.title = raw_book[:title]
+    book.author = raw_book[:author]
+    book.publication_date = raw_book[:publication_date]
+    book.synopsis = raw_book[:synopsis]
+
+    # Could also say:
+    # book.title = params[:book][:title]
+
     if book.save
       redirect_to '/books'
     end
@@ -25,9 +38,34 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    raw_book = params[:book]
+
+    book = Book.find(params[:id])
+
+    # We could follow the same pattern from create
+    # book.title = raw_book[:title]
+    # book.author = raw_book[:author]
+    # book.publication_date = raw_book[:publication_date]
+    # book.synopsis = raw_book[:synopsis]
+
+    # Or we can do it all together with update_attributes
+    book.update_attributes(
+      title: raw_book[:title],
+      author: raw_book[:author],
+      publication_date: raw_book[:publication_date],
+      synopsis: raw_book[:synopsis]
+    )
+
+    # Arbitrary rule: always use update_attributes, not
+    # update. This will pay off later.
+
+    if book.save
+      redirect_to book_path(book)
+    end
   end
 
   def destroy
