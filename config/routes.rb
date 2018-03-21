f29ce625c641f1f7d34bfa70babbe66bce8289cd
Dan Roberts <dan@adadevelopersacademy.org>
2018-03-21 11:56:27 -0700

    Name our routes, add liks everywhere

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/books', to: 'books#index'
-  get '/books/new', to: 'books#new'
+  get '/books', to: 'books#index', as: 'books'
+  get '/books/new', to: 'books#new', as: 'new_book'
   post '/books', to: 'books#create'
 
-  get '/books/:id', to: 'books#show'
-  get '/books/:id/edit', to: 'books#edit'
+  get '/books/:id', to: 'books#show', as: 'book'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
   patch '/books/:id', to: 'books#update'
   delete '/books/:id', to: 'books#destroy'
 end
