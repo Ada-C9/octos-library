b0e8a475191c274907a48130537c2b5595abde10
CheezItMan <chris@adadevelopersacademy.org>
2018-04-02 15:08:23 -0700

    Added instance method

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,15 @@
 class Author < ApplicationRecord
   has_many :books # plural
+  def first_published
+    my_books = self.books.where.not(publication_year: nil)
+    my_books = my_books.order(publication_year: :asc)
+
+    if my_books.length == 0
+      return nil
+    end
+
+    first_book = my_books.first
+
+    return first_book.publication_year
+  end
 end
