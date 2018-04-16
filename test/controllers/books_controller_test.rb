52329dc278a43739cd3b1d9c2a667dbe9e1008b5
Dan Roberts <dan@adadevelopersacademy.org>
2018-04-16 09:53:54 -0700

    Add tests for index

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe BooksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'index' do
+    it 'sends a success response when there are many books' do
+      # Assumptions
+      Book.count.must_be :>, 0
+
+      # Act
+      get books_path
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it 'sends a success response when there are no books' do
+      # Arrange
+      Book.destroy_all
+      # Book.all.length.must_equal 0
+
+      # Act
+      get books_path
+
+      # Assert
+      must_respond_with :success
+    end
+  end
+
+  describe 'new' do
+  end
+
+  describe 'create' do
+  end
 end
